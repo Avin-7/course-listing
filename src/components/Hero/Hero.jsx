f0ddc25f777778cc6f80e7556fd0f55b4d8dd53d
Avin Joy Dcosta <115493294+Avin-7@users.noreply.github.com>
2024-09-01 10:18:09 +0530

    added h1 and p tag

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,7 +2,20 @@ import React from "react";
 
 function Hero() {
   return (
-    <div>Helloooo{/*remove these curly braces and comment then code here  */}</div>
+    <div>
+      <div className=" flex flex-col justify-center align-middle text-white py-56 bg-slate-50">
+        <div className=" text-center font-poppins">
+          <h1 className="text-blue-900 text-5xl my-4 font-semibold px-14 break-words">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          </h1>
+          <p className="text-md break-words px-20 text-zinc-900">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Consequatur optio odit accusantium quo itaque reprehenderit. Quis
+            maxime vitae modi quo quisquam at voluptatibus. Nulla, voluptate!
+          </p>
+        </div>
+      </div>
+    </div>
   );
 }
 
