61a1d8d91b33ad122ce3bd9424c55c9e62b880b3
Avin Joy Dcosta <115493294+Avin-7@users.noreply.github.com>
2024-09-21 21:39:55 +0530

    add skeleton card component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Hero from "../Hero/Hero";
 import Courses from "../Courses/Courses";
 import Card from "../Card/Card";
+import SkeletonCard from "../Card/SkeletonCard";
 function Home() {
   return (
     <div>
       <Hero />
       <Courses />
       <Card />
+      <SkeletonCard />
     </div>
   );
 }
