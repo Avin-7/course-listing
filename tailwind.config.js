46c2acc316ed62516d0fe29e3bf3b4b529cf80f5
Avin Joy Dcosta <115493294+Avin-7@users.noreply.github.com>
2024-09-21 21:40:46 +0530

    add custom animation for skeleton card

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,14 @@ export default {
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
       },
+      animation: {
+        'pulsate': 'pulse 1.2s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+      },
+      keyframes: {
+        pulsate: {
+          '50%': { opacity:' .5'},
+        }
+      }
     },
   },
   plugins: [],
