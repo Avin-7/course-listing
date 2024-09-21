7eddbf7faa73539afc679986b9d4f56793b7f7e9
Avin Joy Dcosta <115493294+Avin-7@users.noreply.github.com>
2024-09-21 21:37:34 +0530

    update heading of the project

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,15 +3,15 @@ import React from "react";
 function Hero() {
   return (
     <div>
-      <div className=" flex flex-col justify-center align-middle text-white py-56 bg-slate-200">
+      <div className=" flex flex-col justify-center align-middle text-white py-40 border bg-gray-50">
         <div className=" text-center font-poppins">
-          <h1 className="text-blue-900 text-6xl my-4 font-semibold mx-32 max-md:mx-20 max-sm:mx-14 max-md:text-4xl break-words tracking-wide">
-            Many Courses, At one Place
+          <h1 className="text-6xl my-4 font-semibold pb-10 mx-32 max-md:mx-20 max-sm:mx-14 max-md:text-4xl break-words tracking-wider bg-clip-text text-transparent bg-gradient-to-r from-purple-500 to-purple-900 ">
+            Curating the best courses for effortless, distraction free learning
           </h1>
-          <p className="text-md break-words text-xl px-28 max-md:px-24 max-sm:px-10 text-zinc-900">
+          {/* <p className="text-md break-words text-lg px-28 max-md:px-24 max-sm:px-10 text-zinc-900">
             Lorem, ipsum dolor sit amet consectetur adipisicing elit.
             Consequatur optio odit accusantium quo itaque reprehenderit.
-          </p>
+          </p> */}
         </div>
       </div>
     </div>
