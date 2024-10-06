c0c60ad0394020c1a3fab88a67af0757b387f597
Avin Joy Dcosta <115493294+Avin-7@users.noreply.github.com>
2024-10-06 12:39:04 +0530

    Minor changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,11 +10,11 @@ export default {
         poppins: ["Poppins", "sans-serif"],
       },
       animation: {
-        'pulsate': 'pulse 1.2s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+        'pulsate': 'pulse 1.5s cubic-bezier(0.4, 0, 0.6, 1) infinite',
       },
       keyframes: {
         pulsate: {
-          '50%': { opacity:' .5'},
+          '50%': { opacity:' .75'},
         }
       }
     },
