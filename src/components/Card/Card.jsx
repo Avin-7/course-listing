f8e3e9e9565291cd055ad99a4d4a0635faa6fcbf
Avin Joy Dcosta <avinjoy51@gmail.com>
2024-10-19 21:55:01 +0530

    add props, wishlist icon,button

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,29 +1,38 @@
-import React from "react";
-import tum from "../../assets/tum.jpg"
+import React, { useState } from "react";
+import tum from "../../assets/tum.jpg";
+import { RxHeart, RxHeartFilled, RxArrowTopRight } from "react-icons/rx";
 
-function Card() {
-  return <div>    <div className="container">
-  <div className="card">
-      <img src={tum} alt=""/>
-      <div className="rating">
-          <i className='bx bx-star'></i>
-          <i className='bx bx-star'></i>
-          <i className='bx bx-star'></i>
-          <i className='bx bx-star'></i>
-          <i className='bx bx-star'></i>
+function Card({ course }) {
+  const [wishlist, setWishlist] = useState(false);
+  return (
+    <div>
+      <div className="container">
+        <div className="card ">
+          <img src={tum} alt="" />
+          <div className="details relative">
+            <div className="btn absolute text-2xl top-4 right-5">
+              {wishlist ? (
+                <RxHeart onClick={() => setWishlist(!wishlist)} />
+              ) : (
+                <RxHeartFilled
+                  className="text-purple-700"
+                  onClick={() => setWishlist(!wishlist)}
+                />
+              )}
+            </div>
+            <h1 className=" text-lg text-balance ">{course.title}</h1>
+            <p className=" my-1">
+             {course.desc}
+            </p>
+            <div className="flex justify-end ml-40 rounded-lg mt-4 px-4 py-3 bg-purple-400 hover:animate-vibrate ease-in-out transition">
+              <button className=" tracking-tight text-md">View more</button>
+              <RxArrowTopRight className="mt-0.5 ml-0.5 text-xl " />
+            </div>
+          </div>
+        </div>
       </div>
-  <div className="details">
-      <h1>JavaScript for beginners</h1>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-      Ut quibusdam tempora similique perspiciatis vero eaque!</p>
-      <hr/>
-      <div className="btn">
-          <i className='bx bx-bookmark'></i>
-      </div>
-  </div>
-</div>
-</div>
-</div>;
+    </div>
+  );
 }
 
 export default Card;
