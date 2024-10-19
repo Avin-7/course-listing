285446662503013b51e112e7814793d1ed3103d0
Avin Joy Dcosta <avinjoy51@gmail.com>
2024-10-19 21:59:24 +0530

    add animation for card button

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,14 +11,19 @@ export default {
       },
       animation: {
         'pulsate': 'pulse 1.5s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+        'vibrate': 'vibrate 1.5s cubic-bezier(0.4, 0, 0.6, 1) infinite',
       },
       keyframes: {
         pulsate: {
           '50%': { opacity:' .75'},
-        }
+        },
+        vibrate: {
+          "0%": { transform: "scale(0.959)"},
+          "50%": { transform: "scale(0.889)"},
+          "100%": { transform: "scale(0.959)"},
       }
     },
   },
   plugins: [],
 }
-
+}
