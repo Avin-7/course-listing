c663db7c0e885cce2b85a463c76451b399eb7948
Avin Joy Dcosta <avinjoy51@gmail.com>
2024-10-19 21:59:58 +0530

    add new component files

---
+++ src/components/Park.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+function park() {
+  return (
+    <div className=" w-96 px-20">
+      <div className=" flex p-1 w-full">
+        <div className=" bg-green-500 m-1 w-5 h-10 rounded-lg"></div>
+        <div className=" bg-green-500 m-1 w-5 h-10 rounded-lg"></div>
+        <div className=" bg-green-500 m-1 w-5 h-10 rounded-lg "></div>
+        <div className=" bg-green-500 m-1 w-5 h-10 rounded-lg"></div>
+        <div className=" bg-green-500 m-1 w-5 h-10 rounded-lg"></div>
+      </div>
+      <div className=" flex p-1 w-full">
+        <div className=" bg-green-500 m-1 w-5 h-10 rounded-lg"></div>
+        <div className=" bg-red-500 m-1 w-5 h-10 rounded-lg"></div>
+        <div className=" bg-green-500 m-1 w-5 h-10 rounded-lg "></div>
+        <div className=" bg-green-500 m-1 w-5 h-10 rounded-lg"></div>
+        <div className=" bg-red-500 m-1 w-5 h-10 rounded-lg"></div>
+      </div>
+      <div className=" flex p-1 w-full">
+        <div className=" bg-green-500 m-1 w-5 h-10 rounded-lg"></div>
+        <div className=" bg-green-500 m-1 w-5 h-10 rounded-lg"></div>
+        <div className=" bg-red-500 m-1 w-5 h-10 rounded-lg "></div>
+        <div className=" bg-green-500 m-1 w-5 h-10 rounded-lg"></div>
+        <div className=" bg-red-500 m-1 w-5 h-10 rounded-lg"></div>
+      </div>
+      <div className=" flex p-1 w-full">
+        <div className=" bg-green-500 m-1 w-5 h-10 rounded-lg"></div>
+        <div className=" bg-green-500 m-1 w-5 h-10 rounded-lg"></div>
+        <div className=" bg-green-500 m-1 w-5 h-10 rounded-lg "></div>
+        <div className=" bg-green-500 m-1 w-5 h-10 rounded-lg"></div>
+        <div className=" bg-red-500 m-1 w-5 h-10 rounded-lg"></div>
+      </div>
+   
+    </div>
+  );
+}
+
+export default park;
