652fc11e5a46b64ab7249821e4cc9eb621f15df0
Avin Joy Dcosta <avinjoy51@gmail.com>
2024-10-27 11:53:22 +0530

    add login functionality

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,43 +1,95 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import Google from "../../assets/Google.png";
+import { useDispatch } from "react-redux";
+import { login as authLogin } from "../../store/authSlice";
+import { useNavigate } from "react-router-dom";
+import authService from "../../appwrite/auth";
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const login = async (e) => {
+    e.preventDefault();
+    try {
+      const user = await authService.login({ email, password });
+      if (user) {
+        dispatch(authLogin(user));
+        alert("Logged in");
+      }
+      navigate("/");
+    } catch (err) {
+      setError(err.message);
+    }
+  };
   return (
     <>
-    <div className='flex items-center justify-center mt-8'>
-      <form action="#" className="w-28"/>
-    <div className="flex bg-white h-5" id="container">
-      <div className="pl-10 mr-5" id="col">
-          <div className="h11">
+      <div className="flex items-center justify-center mt-8">
+        <form className="w-28 form-login " />
+        <div className="flex bg-white h-5" id="container">
+          <div className="pl-10 mr-5" id="col">
+            <div className="h11">
               <h1 className=" pl-19 font-semibold p-4 ">Login</h1>
-          </div>
-          <div className="mt-4" id="input_container">
-              <label htmlfor="email">Email</label>
-              <input type="email" name="Email" id="he" className="bg-transparent outline-none  p-1" required/>
-          </div>
-          <div className="mt-6" id="input_container2">
-              <label htmlfor="password">Password</label>
-              <input type="password" name="password" id="password" className="bg-transparent  mb-5 p-1" required/>
-              <img src="hidden.png" alt="" onclick="pass()" className="pass-icon" id="pass-icon"/>
-          </div>
-          <div className="hl">
-            <a href="">Forgot Password?</a>
-          </div>
-          <div className="relative">
-            <button className="sign_in_btn">Sign In</button>
-          </div>
-          <div className="option2">OR</div>
+            </div>
+            <div className="mt-4" id="input_container">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                name="Email"
+                id="he"
+                value={email}
+                className="bg-transparent outline-none  p-1 input-login"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="mt-6" id="input_container2">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                value={password}
+                className="bg-transparent  mb-5 p-1 input-login"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <img
+                src="hidden.png"
+                alt=""
+                onClick={() => pass()}
+                className="pass-icon img-login"
+                id="pass-icon"
+              />
+            </div>
+            <div className="hl">
+              <a href="">Forgot Password?</a>
+            </div>
+            <div className="relative">
+              <button
+                type="submit"
+                className="sign_in_btn"
+                onClick={(e) => login(e)}
+              >
+                Sign In
+              </button>
+            </div>
+            <div className=" text-center my-2">OR</div>
             <button className="b2">
-              <img src="G.png" alt="" className="size-5"/>
-              <span className="">Sign in  with Google</span>
+              <img src={Google} alt="" className="size-5 img" />
+              <span className="span">Sign up with Google</span>
             </button>
             <div className="sign_up">
-              <p>Dont't have an account <a href="#">Sign-up</a></p>
+              <p>
+                Dont't have an account <a href="#">Sign-up</a>
+              </p>
             </div>
+          </div>
+        </div>
       </div>
-     </div>
-    </div>
     </>
-  )
+  );
 }
 
-export default Login
+export default Login;
