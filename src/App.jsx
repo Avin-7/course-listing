480af2f24cd3945146ea8a7a361fb95d58096fc5
Avin Joy Dcosta <avinjoy51@gmail.com>
2024-10-27 11:51:51 +0530

    add auto login and redux store

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
 import "./App.css";
+import { useEffect } from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
 import { Outlet } from "react-router-dom";
+import authService from "./appwrite/auth";
+import { useDispatch } from "react-redux";
+import { login, logout } from "./store/authSlice";
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    authService.getCurrentUser().then((userData) => {
+      if (userData) {
+        dispatch(login({ userData }));
+      } else {
+        dispatch(logout());
+      }
+    });
+  }, []);
   return (
     <>
       <Navbar />
       <Outlet />
-    
     </>
   );
 }
