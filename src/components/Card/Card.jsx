80a4b28dd70e10acd3c2820f2a43fc7926c1dda1
Avin Joy Dcosta <avinjoy51@gmail.com>
2024-11-03 21:50:56 +0530

    add wishlist feature

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,39 +1,63 @@
-import React, { useState } from "react";
-import tum from "../../assets/tum.jpg";
+import React, { useEffect, useState } from "react";
 import { RxHeart, RxHeartFilled, RxArrowTopRight } from "react-icons/rx";
-
+import { useNavigate, Link } from "react-router-dom";
+import service from "../../appwrite/config";
+import { useDispatch, useSelector } from "react-redux";
+import { addToWishlist, removeFromWishlist } from "../../store/authSlice";
 
 function Card({ course }) {
-  const [wishlist, setWishlist] = useState(false);
-  return (
+  const dispatch = useDispatch();
+  const [wishlisted, setWishlisted] = useState(course.wishlisted);
+  useEffect(() => {
+    service.updateWishlistStatus(course.$id, wishlisted);
+  }, [wishlisted]);
+  return course ? (
     <div>
       <div className="container">
-        <div className="card ">
-          <img src={tum} alt="" />
+        <div className="card rounded-3xl">
+          <div className=" p-4 rounded-2xl">
+            <img
+              src={service.getFilePreiview(course.image)}
+              alt={course.title}
+              className=" rounded-2xl p-6 object-cover"
+            />
+          </div>
           <div className="details relative">
-            <div className="btn absolute text-2xl top-4 right-5">
-              {wishlist ? (
-                <RxHeart onClick={() => setWishlist(!wishlist)} />
+            <div className="btn absolute text-2xl top-2 right-5">
+              {!wishlisted ? (
+                <RxHeart
+                  onClick={(e) => {
+                    setWishlisted(!wishlisted);
+                    dispatch(addToWishlist(course.$id));
+                  }}
+                />
               ) : (
                 <RxHeartFilled
-                  className="text-purple-700"
-                  onClick={() => setWishlist(!wishlist)}
+                  className="text-[#f21b3f]"
+                  onClick={() => {
+                    setWishlisted(!wishlisted);
+                    dispatch(removeFromWishlist(course.$id));
+                  }}
                 />
               )}
             </div>
-            <h1 className=" text-lg text-balance ">{course.title}</h1>
-            <p className=" my-1">
-             {course.desc}
-            </p>
-            <div className="flex justify-end ml-36 rounded-lg mt-4 px-4 py-3 bg-purple-400 hover:animate-vibrate ease-in-out transition">
-              <button className=" tracking-tight text-md">View more</button>
-              <RxArrowTopRight className="mt-0.5 ml-0.5 text-xl " />
-            </div>
+            <h1 className="text-base text-balance font-poppins leading-[1.7rem]">
+              {course.name}
+            </h1>
+            <p className=" my-1 font-roboto">{course.description}</p>
+            <Link to={`/course/${course.$id}`}>
+              <div className="flex justify-center align-middle gap-0.5 rounded-xl mt-4 px-4 py-3 bg-purple-400 hover:animate-vibrate w-full ease-in-out transition">
+                <button className=" tracking-tight text-md ">
+                  <span>View more</span>
+                </button>
+                <RxArrowTopRight className="mt-0.5 text-xl " />
+              </div>
+            </Link>
           </div>
         </div>
       </div>
     </div>
-  );
+  ) : null;
 }
 
 export default Card;
