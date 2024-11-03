55c420044c6856d8a1a5ac4bce0ec68569cae7e9
Avin Joy Dcosta <avinjoy51@gmail.com>
2024-11-03 21:43:18 +0530

    add figtree font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        test: ["Playwrite TZ"],
+        playwrite: ["Playwrite TZ", "sans-serif"],
         poppins: ["Poppins", "sans-serif"],
-        roboto:["Roboto","sans-serif"],
-
+        roboto: ["Roboto", "sans-serif"],
+        pacifico: ["Pacifico", "sans-serif"],
+        figtree: ["Figtree", "sans-serif"],
       },
       animation: {
-        'pulsate': 'pulse 1.5s cubic-bezier(0.4, 0, 0.6, 1) infinite',
-        'vibrate': 'vibrate 0.5s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+        pulsate: "pulse 1.5s cubic-bezier(0.4, 0, 0.6, 1) infinite",
+        vibrate: "vibrate 1.5s cubic-bezier(0.4, 0, 0.6, 1) infinite",
       },
       keyframes: {
         pulsate: {
-          '50%': { opacity:' .75'},
+          "50%": { opacity: " .75" },
         },
         vibrate: {
-          "0%": { transform: "scale(0.959)"},
-          "50%": { transform: "scale(0.940)"},
-          "100%": { transform: "scale(0.959)"},
-      }
+          "0%": { transform: "scale(0.959)" },
+          "50%": { transform: "scale(0.940)" },
+          "100%": { transform: "scale(0.959)" },
+        },
+      },
     },
+    plugins: [],
   },
-  plugins: [],
-}
-}
+};
