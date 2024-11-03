457854d2022ab56cdd3f00a24ba2b047c31d52b9
Avin Joy Dcosta <avinjoy51@gmail.com>
2024-11-03 21:49:08 +0530

    add wishlist and loading animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,61 @@
 import "./App.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import authService from "./appwrite/auth";
-import { useDispatch } from "react-redux";
-import { login, logout } from "./store/authSlice";
+import service from "./appwrite/config";
+import { useDispatch, useSelector } from "react-redux";
+import conf from "./conf/conf";
+import { login, logout, storeWishlist } from "./store/authSlice";
+import { adminlogin } from "./store/adminSlice";
+
 function App() {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const user = useSelector((state) => state.auth.userData);
+  const wishlistData = useSelector((state) => state.auth.wishlistData);
+  const userId = useSelector((state) => state.auth.userId);
+  const wishlistId = useSelector((state) => state.auth.wishlistId);
+
   useEffect(() => {
     authService.getCurrentUser().then((userData) => {
       if (userData) {
         dispatch(login({ userData }));
+        const userId = userData.$id;
+        service.getWishlists(userId).then((res) => {
+          if (res) {
+            // console.log(res);
+            const wishlist = res.documents[0].wishlist;
+            const wishlistId = res.documents[0].$id;
+            dispatch(storeWishlist({ userId, wishlist, wishlistId }));
+          }
+        });
       } else {
         dispatch(logout());
       }
+      if (userData.email == conf.adminEmailId.toLowerCase()) {
+        dispatch(adminlogin(true));
+      }
     });
   }, []);
+
+  useEffect(() => {
+    service.updateWishlist(wishlistId, userId, wishlistData);
+  }, [wishlistData]);
+
+  const loading = () => {
+    navigate("/loading");
+    setTimeout(function () {
+      navigate("/");
+    }, 2000);
+  };
+
+  useEffect(() => {
+    loading();
+  }, [user]);
+
   return (
     <>
       <Navbar />
