fad5af87f4483a3325646018614d25a7d3871bf0
Avin Joy Dcosta <avinjoy51@gmail.com>
2025-01-27 12:02:37 +0530

    added view courses component

--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -1,12 +1,39 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Form from "./Form";
 import Table from "../Admin/Table";
+import ViewCourses from "./ViewCourses";
+import service from "../../appwrite/config.js";
+
 function Admin() {
-  const [showAddCourseForm, setShowAddCourseForm] = useState(false);
+  const [courses, setCourses] = useState([]);
+
+  const [component, setComponent] = useState({
+    view: false,
+    add: false,
+  });
 
   const formToAdmin = (data) => {
-    setShowAddCourseForm(data);
+    setComponent(!data);
   };
+
+  const handleComponents = () => {
+    if (component.view) {
+      return <ViewCourses courses={courses} />;
+    } else if (component.add) {
+      return <Form formToAdmin={formToAdmin} viewm={component.view} />;
+    }
+  };
+  useEffect(() => {
+    service
+      .getCourses()
+      .then((data) => {
+        console.log(data);
+        setCourses(data.documents);
+      })
+      .catch((error) => console.log("Error in data fetch" + error))
+      .finally(() => {});
+  }, [component.view]);
+
   return (
     <div className="font-poppins tracking-wide">
       <div className="w-[96%]">
@@ -14,28 +41,27 @@ function Admin() {
           <div>
             <button
               onClick={() => {
-                setShowAddCourseForm((prev) => !prev);
+                setComponent({ view: false, add: true });
               }}
               className=" px-4 py-3 bg-blue-500 text-white text-sm font-semibold rounded-md"
-              disabled={showAddCourseForm}
             >
               Add course
             </button>
           </div>
-          <div className=" w-11/12 ">
-            {showAddCourseForm ? (
-              <div className=" bg-slate-50 rounded-md p-1">
-                <Form
-                  formToAdmin={formToAdmin}
-                  showAddCourseForm={showAddCourseForm}
-                />
-              </div>
-            ) : null}
+          <div>
+            <button
+              onClick={() => {
+                setComponent({ view: !component.view, add: false });
+              }}
+              className=" px-4 py-3 bg-blue-500 text-white text-sm font-semibold rounded-md"
+            >
+              View courses
+            </button>
           </div>
         </div>
-      </div>
-      <div className="">
-        <Table />
+        <div className=" w-[98%] ">
+          {handleComponents()}
+        </div>
       </div>
     </div>
   );
