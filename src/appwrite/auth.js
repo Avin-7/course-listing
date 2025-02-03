6b6ad635f5faaa932b47d90e897f6259f3c79ab6
Avin Joy Dcosta <avinjoy51@gmail.com>
2025-02-03 21:48:31 +0530

    added forgot and reset password utility functions

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -39,6 +39,29 @@ export class AuthService {
     }
   }
 
+  async forgotPassword({ email }) {
+    try {
+      return await this.account.createRecovery(
+        email,
+        "http://localhost:5173/reset-password"
+      );
+    } catch (error) {
+      console.log("ERROR in forgot password", error);
+    }
+  }
+  async resetPassword({ userId, secret, password }) {
+    try {
+      return await this.account.updateRecovery(
+        userId,
+        secret,
+        password.newPassword,
+        password.repeatPassword
+      );
+    } catch (error) {
+      console.log("ERROR in reset password", error);
+    }
+  }
+
   async getCurrentUser() {
     try {
       return await this.account.get();
