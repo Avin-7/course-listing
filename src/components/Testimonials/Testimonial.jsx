a16632a879283918ab527b5ac5118c0b04e99ca4
Avin Joy Dcosta <avinjoy51@gmail.com>
2025-02-16 19:59:14 +0530

    modified UI of the testimonial component

--- src/components/Testimonials/Testimonial.jsx
+++ src/components/Testimonials/Testimonial.jsx
@@ -78,103 +78,119 @@ function Testimonial() {
             </div>
           </div> */}
 
-        <div className="container mx-auto">
-          <h2 className="text-3xl font-bold mb-2 text-center">Testimonials</h2>
-          <div className="masonry overflow-hidden">
+        <div className="mx-auto bg-neutral-900 py-28">
+          <h2 className="text-3xl text-white font-bold mb-20 text-center font-poppins">
+            Testimonials
+          </h2>
+          <div className="mb-10 masonry overflow-hidden">
             <div className="marquee-wrapper">
-              <div className="marquee-item masonry-item  bg-white p-6 rounded-lg shadow-lg">
-                <p className="text-gray-700">
+              <div className="marquee-item masonry-item bg-gradient-to-br from-neutral-900 via-neutral-800 to-neutral-900 p-6 rounded-lg shadow-lg">
+                <p className="text-gray-300 font-figtree ">
                   "Found the perfect course in minutes!"
                 </p>
-                <h3 className="mt-4 text-xl font-semibold">Amit S.</h3>
-                <span className="text-sm text-gray-500">Student</span>
+                <h3 className="mt-4 text-xl font-semibold text-white">
+                  Amit S.
+                </h3>
+                <span className="text-sm text-stone-300">Student</span>
               </div>
-              <div className="marquee-item masonry-item bg-white p-6 rounded-lg shadow-lg">
-                <p className="text-gray-700">
+              <div className="marquee-item masonry-item bg-gradient-to-br from-neutral-900 via-neutral-800 to-neutral-900 p-6 rounded-lg shadow-lg ">
+                <p className="text-gray-300 font-figtree">
                   "No more endless searching for quality courses!"
                 </p>
-                <h3 className="mt-4 text-xl font-semibold">Sarah L.</h3>
-                <span className="text-sm text-gray-500">Freelancer</span>
+                <h3 className="mt-4 text-xl text-white font-DMsans">
+                  Sarah L.
+                </h3>
+                <span className="text-sm text-stone-300">Freelancer</span>
               </div>
-              <div className="marquee-item masonry-item bg-white p-6 rounded-lg shadow-lg">
-                <p className="text-gray-700">
+              <div className="marquee-item masonry-item bg-gradient-to-br from-neutral-900 via-neutral-800 to-neutral-900 p-6 rounded-lg shadow-lg">
+                <p className="text-gray-300 font-figtree">
                   "Filters out the noise, love it!"
                 </p>
-                <h3 className="mt-4 text-xl font-semibold">Jason</h3>
-                <span className="text-sm text-gray-500">Software Engineer</span>
+                <h3 className="mt-4 text-xl text-white font-DMsans">Jason</h3>
+                <span className="text-sm text-stone-300">Software Engineer</span>
               </div>
               {/* <!-- Add more testimonials as needed --> */}
               {/* <!-- Duplicate the testimonials to create the seamless scrolling effect --> */}
-              <div className="marquee-item masonry-item bg-white p-6 rounded-lg shadow-lg">
-                <p className="text-gray-700">
+              <div className="marquee-item masonry-item bg-gradient-to-br from-neutral-900 via-neutral-800 to-neutral-900 p-6 rounded-lg shadow-lg">
+                <p className="text-gray-300 font-figtree">
                   "The service was excellent. Highly recommend!"
                 </p>
-                <h3 className="mt-4 text-xl font-semibold">John Doe</h3>
-                <span className="text-sm text-gray-500">Tutor</span>
+                <h3 className="mt-4 text-xl font-DMsans text-white">
+                  John Doe
+                </h3>
+                <span className="text-sm text-stone-300">Tutor</span>
               </div>
-              <div className="marquee-item masonry-item bg-white p-6 rounded-lg shadow-lg">
-                <p className="text-gray-700">"Saves me time and money!"</p>
-                <h3 className="mt-4 text-xl font-semibold">Daniel </h3>
-                <span className="text-sm text-gray-500">Freelancer</span>
+              <div className="marquee-item masonry-item bg-gradient-to-br from-neutral-900 via-neutral-800 to-neutral-900 p-6 rounded-lg shadow-lg">
+                <p className="text-gray-300 font-figtree">
+                  "Saves me time and money!"
+                </p>
+                <h3 className="mt-4 text-xl font-DMsans text-white">
+                  Daniel{" "}
+                </h3>
+                <span className="text-sm text-stone-300">Freelancer</span>
               </div>
-              <div className="marquee-item masonry-item bg-white p-6 rounded-lg shadow-lg">
-                <p className="text-gray-700">
+              <div className="marquee-item masonry-item bg-gradient-to-br from-neutral-900 via-neutral-800 to-neutral-900 p-6 rounded-lg shadow-lg">
+                <p className="text-gray-300 font-figtree">
                   "I would definitely use their services again."
                 </p>
-                <h3 className="mt-4 text-xl font-semibold">Sarah Lee</h3>
-                <span className="text-sm text-gray-500">Graphic Designer</span>
+                <h3 className="mt-4 text-xl font-DMsans text-white">
+                   Sachin
+                </h3>
+                <span className="text-sm text-stone-300">Graphic Designer</span>
               </div>
             </div>
           </div>
           <div className="masonry overflow-hidden">
             <div className="marquee-wrapper-left">
-              <div className="marquee-item masonry-item bg-white p-6 rounded-lg shadow-lg">
-                <p className="text-gray-700">
+              <div className="marquee-item masonry-item bg-gradient-to-br from-neutral-900 via-neutral-800 to-neutral-900 p-6 rounded-lg shadow-lg">
+                <p className="text-gray-300 font-figtree">
                   "Learnt React faster, thanks to DotLib!"
                 </p>
-                <h3 className="mt-4 text-xl font-semibold">Rahul C. </h3>
-                <span className="text-sm text-gray-500">
-                  Full Stack Developer
-                </span>
+                <h3 className="mt-4 text-xl font-DMsans text-white">
+                  Rahul C.{" "}
+                </h3>
+                <span className="text-sm text-stone-300">Full Stack Developer</span>
               </div>
-              <div className="marquee-item masonry-item bg-white p-6 rounded-lg shadow-lg">
-                <p className="text-gray-700">
+              <div className="marquee-item masonry-item bg-gradient-to-br from-neutral-900 via-neutral-800 to-neutral-900 p-6 rounded-lg shadow-lg">
+                <p className="text-gray-300 font-figtree">
                   "Great for finding niche tech courses!"
                 </p>
-                <h3 className="mt-4 text-xl font-semibold">Meera</h3>
-                <span className="text-sm text-gray-500">AI/ML Researcher</span>
+                <h3 className="mt-4 text-xl font-DMsans text-white">Meera</h3>
+                <span className="text-sm text-stone-300">AI/ML Researcher</span>
               </div>
-              <div className="marquee-item masonry-item bg-white p-6 rounded-lg shadow-lg">
-                <p className="text-gray-700">
+              <div className="marquee-item masonry-item bg-gradient-to-br from-neutral-900 via-neutral-800 to-neutral-900 p-6 rounded-lg shadow-lg">
+                <p className="text-gray-300 font-figtree">
                   "Saves hours of searching for good courses!"
                 </p>
-                <h3 className="mt-4 text-xl font-semibold">Arjun</h3>
-                <span className="text-sm text-gray-500">Freelancer</span>
+                <h3 className="mt-4 text-xl font-DMsans text-white">Arjun</h3>
+                <span className="text-sm text-stone-300">Freelancer</span>
               </div>
               {/* <!-- Add more testimonials as needed --> */}
               {/* <!-- Duplicate the testimonials to create the seamless scrolling effect --> */}
-              <div className="marquee-item masonry-item bg-white p-6 rounded-lg shadow-lg">
-                <p className="text-gray-700">
+              <div className="marquee-item masonry-item bg-gradient-to-br from-neutral-900 via-neutral-800 to-neutral-900 p-6 rounded-lg shadow-lg">
+                <p className="text-gray-300 font-figtree">
                   "No more searching, just learning!"
                 </p>
-                <h3 className="mt-4 text-xl font-semibold">Karthik</h3>
-                <span className="text-sm text-gray-500">Tutor</span>
+                <h3 className="mt-4 text-xl font-DMsans text-white">
+                  Karthik
+                </h3>
+                <span className="text-sm text-stone-300">Tutor</span>
               </div>
-              <div className="marquee-item masonry-item bg-white p-6 rounded-lg shadow-lg">
-                <p className="text-gray-700">
+              <div className="marquee-item masonry-item bg-gradient-to-br from-neutral-900 via-neutral-800 to-neutral-900 p-6 rounded-lg shadow-lg">
+                <p className="text-gray-300 font-figtree">
                   "Easiest way to discover top design courses!"{" "}
                 </p>
-                <h3 className="mt-4 text-xl font-semibold">Neha </h3>
-                <span className="text-sm text-gray-500">UI/UX Designer</span>
+                <h3 className="mt-4 text-xl font-DMsans text-white">Neha </h3>
+                <span className="text-sm text-stone-300">UI/UX Designer</span>
               </div>
-              <div className="marquee-item masonry-item bg-white p-6 rounded-lg shadow-lg">
-                <p className="text-gray-700">
+              <div className="marquee-item masonry-item bg-gradient-to-br from-neutral-900 via-neutral-800 to-neutral-900 p-6 rounded-lg shadow-lg">
+                <p className="text-gray-300 font-figtree">
                   "Found the perfect coding course instantly!"{" "}
                 </p>
-                <h3 className="mt-4 text-xl font-semibold">Vikram P. </h3>
-                <span className="text-sm text-gray-500">
-                  Software Developer
-                </span>
+                <h3 className="mt-4 text-xl font-DMsans text-white">
+                  Vikram P.{" "}
+                </h3>
+                <span className="text-sm text-stone-300">Software Developer</span>
               </div>
             </div>
           </div>
