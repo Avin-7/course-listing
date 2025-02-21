2641c6e540ea293f7d8f11a274cbff348b87bc37
Astern Pinto <asternp26@gmail.com>
2025-02-21 21:27:38 +0530

    solved footer issue

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Hero from "../Hero/Hero";
 import Courses from "../Courses/Courses";
 import Testimonial from "../Testimonials/Testimonial";
-import Footer from "../Footer/footer";
+import Footer from "../Footer/Footer";
 function Home() {
 
   return (
