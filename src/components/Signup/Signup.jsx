e1ceac56e7f51b3cc0d3b2e6eb497a8f47eead6f
Avin Joy Dcosta <avinjoy51@gmail.com>
2025-03-17 19:19:51 +0530

    modified the ui of signup page to give it a darker look

--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Google from "../../assets/Google.png";
 import authService from "../../appwrite/auth";
 import { useNavigate } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
+import { useDispatch } from "react-redux";
 import { login as authLogin, storeWishlist } from "../../store/authSlice";
 import { adminlogin } from "../../store/adminSlice";
 import conf from "../.././conf/conf";
@@ -28,7 +28,6 @@ function Signup() {
             });
             if (user) {
               dispatch(authLogin(user));
-              // console.log(user);
               const userId = user.userId;
               const wishlist = [];
               const res = await service.createWishlist({ userId, wishlist });
@@ -64,31 +63,25 @@ function Signup() {
   };
   return (
     <>
-      <div className="text-black flex justify-center items-center w-full bg-gradient-to-br from-fuchsia-600  via-purple-600 to-indigo-500 py-5">
-        <div className=" w-2/4 max-md:hidden text-left flex justify-center ">
-          <div className="py-52 text-white ">
-            <h1 className=" text-7xl font-figtree font-extralight tracking-wide text-balance px-20 break-words mb-3">
-              Create
+      <div className="text-black flex justify-center items-center w-full bg-gradient-to-br bg-neutral-900 py-5 pt-24">
+        <div className=" w-[35%] max-md:hidden text-left flex justify-center ">
+          <div className="py-52 ml-36">
+            <h1 className=" text-7xl max-lg:text-5xl tracking-wide text-balance px-10 py-2 break-words mb-3 text-transparent bg-clip-text bg-gradient-to-br from-purple-800 via-pink-600 to-purple-900  font-poppins font-semibold">
+              Create new account.
             </h1>
-            <h3 className=" text-7xl font-figtree tracking-wide font-extralight  text-balance px-20 mb-3">
-              your new
-            </h3>
-            <h2 className="text-7xl font-figtree tracking-wide font-extralight  text-balance px-20 mb-1">
-              account.
-            </h2>
           </div>
         </div>
-        <div className="w-2/4 flex justify-center">
-          <form>
-            <div className="w-80 max-[375px]:w-[18rem]  bg-white py-1 px-5 rounded-xl">
-              <div className="pl-8 max-[375px]:pl-5 mr-5">
-                <div className=" text-4xl -ml-4 my-6 w-full">
-                  <h1 className=" text-3xl text-black p-5 font-semibold ml-10 font-playwrite bg-clip-text text-transparent bg-gradient-to-r from-purple-900 to-purple-400">
+        <div className="w-[65%] max-lg:w-11/12 max-[375px]:w-[94%] flex justify-center"> 
+          <form className=" w-[50%] max-xl:w-[65%] max-md:w-[90%]">
+            <div className="w-full bg-neutral-800 py-1 px-5 max-sm:px-1 rounded-xl">
+              <div className="pl-8 max-lg:pl-4 max-[375px]:pl-4 mr-5">
+                <div className=" text-4xl -ml-4 my-6 ">
+                  <h1 className=" text-3xl text-black p-5 font-semibold text-center font-playwrite bg-clip-text text-transparent bg-gradient-to-r from-purple-800 via-pink-600 to-purple-900">
                     DotLib
                   </h1>
                 </div>
-                <div className="mt-4 mb-6">
-                  <label htmlFor="email" className=" font-normal mb-2">
+                <div className="mt-4 mb-6 text-white font-poppins">
+                  <label htmlFor="email" className=" font-normal mb-2 tracking-wider">
                     Name
                   </label>
                   <input
@@ -96,12 +89,12 @@ function Signup() {
                     name="Name"
                     value={name}
                     id="he"
-                    className="bg-transparent border-b-[2px] border-b-neutral-800 py-2 outline-none w-[98%] focus:border-b-blue-700"
+                    className="bg-transparent border-b-[2px] py-2 outline-none w-[98%]  border-b-purple-700 focus:border-b-purple-400 text-white transition-all ease-in"
                     onChange={(e) => setName(e.target.value)}
                   />
                 </div>
-                <div className="mt-4 mb-6">
-                  <label htmlFor="email" className=" font-normal mb-2">
+                <div className="mt-4 mb-6 text-white font-poppins">
+                  <label htmlFor="email" className=" font-normal mb-2 tracking-wider">
                     Email
                   </label>
                   <input
@@ -109,12 +102,12 @@ function Signup() {
                     name="Email"
                     value={email}
                     id="he"
-                    className="bg-transparent border-b-[2px] border-b-neutral-800 py-2 outline-none w-[98%] focus:border-b-blue-700"
+                    className="bg-transparent border-b-[2px] py-2 outline-none w-[98%]  border-b-purple-700 focus:border-b-purple-400 text-white transition-all ease-in"
                     onChange={(e) => setEmail(e.target.value)}
                   />
                 </div>
-                <div className="mt-4 mb-6">
-                  <label htmlFor="password" className=" font-normal mb-6">
+                <div className="mt-4 mb-6 text-white font-poppins">
+                  <label htmlFor="password" className=" font-normal mb-6 tracking-wider">
                     Password
                   </label>
                   <input
@@ -122,7 +115,7 @@ function Signup() {
                     name="password"
                     id="password"
                     value={password}
-                    className="bg-transparent border-b-[2px] border-b-neutral-800 py-2 outline-none w-[98%] focus:border-b-blue-700"
+                    className="bg-transparent border-b-[2px] py-2 outline-none w-[98%]  border-b-purple-700 focus:border-b-purple-400 text-white transition-all ease-in"
                     onChange={(e) => setPassword(e.target.value)}
                   />
                 </div>
@@ -132,17 +125,17 @@ function Signup() {
                   </h2>
                 </div>
 
-                <div className="relative">
+                <div className="relative text-white font-poppins">
                   <button
                     type="submit"
-                    className=" w-[98%] my-5 bg-[#2121ad] text-white py-3 px-2 rounded-xl hover:bg-[#2121adee] transition ease-linear"
+                    className=" w-[98%] my-5 rounded-xl hover:bg-[#2121adee] bg-violet-900 text-white py-3 px-2  hover:bg-violet-800 transition ease-linear tracking-wider"
                     onClick={(e) => signup(e)}
                   >
                     Create new account
                   </button>
                 </div>
                 <div className="option ">OR</div>
-                <button className="w-[98%] flex justify-center items-center gap-2 my-10 py-3 px-2 border-b-2 border-blue-700 rounded-xl bg-gray-200 hover:border-blue-900 hover:bg-neutral-100 transition-all ease-linear">
+                <button className="w-full flex justify-center items-center text-white gap-2 my-10 py-3 px-2 border-2 border-purple-700 rounded-xl bg-neutral-800 transition-all ease-linear font-poppins">
                   <img src={Google} alt="" className="size-5 img" />
                   <span className="span">Signup with Google</span>
                 </button>
