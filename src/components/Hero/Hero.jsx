0898554e67c22d8eceae6a07685b851f5c364f0a
Astern <asternp26@gmail.com>
2025-03-20 18:01:58 +0530

    Updated Hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,57 +1,83 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
-function Hero(){
+function Hero (){
+  const [state, setState] = useState(false)
+  const navigation = [
+      { title: "Features", path: "javascript:void(0)" },
+      { title: "Integrations", path: "javascript:void(0)" },
+      { title: "Customers", path: "javascript:void(0)" },
+      { title: "Pricing", path: "javascript:void(0)" }
+  ]
+  
+  useEffect(() => {
+      document.onclick = (e) => {
+          const target = e.target;
+          if (!target.closest(".menu-btn")) setState(false);
+      };
+    }, [])
+    const Brand = () => (
+        <div className="flex items-center justify-between py-5 md:block">
+            <a href="javascript:void(0)">
+                <img
+                    src="https://www.floatui.com/logo-dark.svg"
+                    width={120}
+                    height={50}
+                    alt="Float UI logo"
+                />
+            </a>
+            <div className="md:hidden">
+                <button className="menu-btn text-gray-400 hover:text-gray-300"
+                    onClick={() => setState(!state)}
+                >
+                    {
+                        state ? (
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
+                                <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
+                            </svg>
+                        ) : (
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                            </svg>
+                        )
+                    }
+                </button>
+            </div>
+        </div>
+    )
   return (
     <div>
-      <div className=" flex flex-col relative justify-center align-middle  h-screen" id="bgimg44 ">
-      <div className="absolute right-top-10">
-        <div className="bg-purple-500 w-72 h-72 rounded-full top-0 -left-4 absolute mix-blend-multiply filter blur-3xl"> 
+       <div className="py-24 bg-gray-900">
+         
+            <section className="relative">
+                <div className="relative z-10 max-w-screen-xl mx-auto px-4 py-28 md:px-8">
+                    <div className="space-y-5 max-w-4xl mx-auto text-center">
+                        <h2 className="text-4xl max-md:text-3xl text-white font-extrabold mx-auto md:text-5xl">
+                        No more jumping <br />between platforms 
+
+                        </h2>
+                        <p className="max-w-2xl mx-auto text-gray-400">
+                        find everything you need right here for focused, <br />hassle-free learning.
+                        </p>
+                        
+                        <div className="flex justify-center items-center gap-x-4 text-gray-400 text-sm">
+                            <div className="flex">
+                                <svg className="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20"><path d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z" /></svg>
+                                <svg className="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20"><path d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z" /></svg>
+                                <svg className="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20"><path d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z" /></svg>
+                                <svg className="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20"><path d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z" /></svg>
+                                <svg className="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20"><path d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z" /></svg>
+                            </div>
+                            <p><span className="text-gray-100">5.0</span> by over 200 users</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="absolute inset-0 m-auto max-w-xs h-[357px] blur-[118px] sm:max-w-md md:max-w-lg" style={{ background: "linear-gradient(106.89deg, rgba(192, 132, 252, 0.11) 15.73%, rgba(14, 165, 233, 0.41) 15.74%, rgba(232, 121, 249, 0.26) 56.49%, rgba(79, 70, 229, 0.4) 115.91%)" }}></div>
+                <div className="absolute inset-0 m-auto max-w-xs h-[357px] blur-[118px] sm:max-w-md md:max-w-lg" style={{ background: "linear-gradient(106.89deg, rgba(192, 132, 252, 0.11) 15.73%, rgba(14, 165, 233, 0.41) 15.74%, rgba(232, 121, 249, 0.26) 56.49%, rgba(79, 70, 229, 0.4) 115.91%)" }}></div>
+            </section>
         </div>
-        <div className="bg-yellow-300 w-72 h-72 rounded-full top-10 left-32 absolute mix-blend-multiply filter blur-3xl"> 
-        </div>
-        <div className="bg-pink-300 w-72 h-72 rounded-full top-17 left-9 absolute mix-blend-multiply filter blur-3xl"> 
-        </div>
-        </div>
-        <div className="absolute right-96 top-24">
-        <div className="bg-purple-400 w-72 h-72 rounded-full top-0 -left-4 absolute mix-blend-multiply filter blur-3xl"> 
-        </div>
-        <div className="bg-yellow-300 w-56 h-72 rounded-full top-10 left-32 absolute mix-blend-multiply filter blur-3xl"> 
-        </div>
-        <div className="bg-pink-300 w-56 h-72 rounded-full top-17 left-9 absolute mix-blend-multiply filter blur-3xl"> 
-        </div>
-        </div>
-        <div className=" font-poppins relative  h-96 bottom-3 ">
-          <h1 className="text-6xl my-8 font-semibold pb-10 mx-32 max-md:mx-20 max-sm:mx-8 max-md:text-4xl break-words tracking-wider bg-clip-text   mb-20 drop-shadow-md text-transparent bg-black ">
-          No more jumping <br />between platforms 
-          <p className="text-base">find everything you need right here for focused, <br />hassle-free learning.</p>
-          </h1>
-          <div className="bg-white/60 h-72 right-32 top-0 w-48 bottom-10 absolute rounded-xl shadow-2xl transform transition-all hover:-translate-y-2 duration-200">
-          <div className="h-32 bg-white/30 rounded-xl border-none">
-              <img src="https://images.pexels.com/photos/11035474/pexels-photo-11035474.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" className="p-2 rounded-lg opacity-60" />
-          </div>
-          <h1 className="text-center font-poppins font-bold">The Complete Python Developer</h1>
-          <button className="bg-blue-300 h text-sm p-2 justify-center flex rounded-md ml-24 text-white/70 mt-16">view more</button>
-        </div>
-        <div className="bg-white/90 h-72 right-56 top-10 w-48 bottom-10 absolute rounded-xl shadow-2xl transform transition-all hover:-translate-y-2 duration-200">
-          <div className="h-32 bg-white/90 rounded-xl">
-              <img src="https://images.pexels.com/photos/11035471/pexels-photo-11035471.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" className="opacity-90 p-2 rounded-lg " />
-          </div>
-          <h1 className="text-center font-poppins font-bold">React - The Complete Guide 2025 (incl. Next.js, Redux)</h1>
-          <button className="bg-blue-400 h text-sm p-2 justify-center flex rounded-md ml-24 text-white/90 mt-10">view more</button>
-        </div>
-        <div className="bg-white h-72  top-20 w-48  right-80 absolute rounded-xl shadow-2xl transform transition-all hover:-translate-y-2 duration-300">
-          <div className="h-32 bg-white rounded-xl">
-              <img src="https://images.pexels.com/photos/11035539/pexels-photo-11035539.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" className=" rounded-lg p-2 " />
-          </div>
-          <h1 className="text-center font-poppins font-bold">Git Complete: The definitive, step-by-step guide to Git</h1>
-          
-          <button className="bg-blue-600  text-sm p-2 justify-center flex rounded-md ml-24 text-white mt-10">view more</button>
-        </div>
-        </div>
-        <button className="rounded-full font-sans btn-hover-animation color-7 absolute bottom-36 left-64 h-16 shadow-lg">
-                Get started
-              </button>
-      </div>
+    
+
     </div>
   );
 }
