49011b56cb1a4d045831d1256f934433c9cda1e9
Avin Joy Dcosta <avinjoy51@gmail.com>
2025-03-20 11:24:04 +0530

    created a new slice named courseSlice

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "./authSlice";
 import adminSlice from "./adminSlice";
+import courseSlice from "./courseSlice";
 const store = configureStore({
   reducer: {
     auth: authSlice,
     adminauth: adminSlice,
+    courseData: courseSlice,
   },
 });
 
