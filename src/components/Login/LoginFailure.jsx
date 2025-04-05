599780aab8d28a8d46904c4c9c2f475bfa51da87
Avin Joy Dcosta <avinjoy51@gmail.com>
2025-04-05 21:18:24 +0530

    added code to navigate user after failed  authentication

--- src/components/Login/LoginFailure.jsx
+++ src/components/Login/LoginFailure.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function LoginFailure() {
+  const navigate = useNavigate();
+  setTimeout(() => {
+    navigate("/");
+  }, 800);
   return (
     <div className=" mt-56 -ml-7 w-full ">
-      <h1 className=" text-center text-white text-2xl font-poppins">Login successful!!</h1>
+      <h1 className=" text-center text-white text-2xl font-poppins">
+        Login Failed!!
+      </h1>
     </div>
   );
 }
