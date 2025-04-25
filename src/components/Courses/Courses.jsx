642df308c5d130981a7181b7eb9ee071014fc685
Avin Joy Dcosta <avinjoy51@gmail.com>
2025-04-25 22:14:49 +0530

    modified the courses file ui

--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -1,49 +1,39 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import SkeletonCard from "../Card/SkeletonCard";
 import Card from "../Card/Card.jsx";
 import { RxArrowRight } from "react-icons/rx";
-import service from "../../appwrite/config.js";
 import SearchBar from "./SearchBar.jsx";
 import Filter from "./Filter.jsx";
-import { storeData } from "../../store/courseSlice.js";
 function Courses() {
   const courses = useSelector((state) => state.courseData.courses);
-  const user = useSelector((state) => state.auth.status);
-  const admin = useSelector((state) => state.adminauth.status);
-  const wishlist = useSelector((state) => state.auth.wishlistData);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    service
-      .getCourses()
-      .then((courses) => {
-        dispatch(storeData(courses.documents));
-      })
-      .catch((error) => console.log("Error in data fetch" + error))
-  }, [user, admin, wishlist]);
 
   return (
-    <div id="courses" className=" bg-gray-900 font-poppins max-sm:8 pt-4 pb-20  px-5">
+    <div
+      id="courses"
+      className=" bg-gray-900 font-poppins max-sm:8 pt-4 pb-20 px-5"
+    >
       <h1 className=" text-center font-bold text-3xl max-md:text-2xl pt-14 text-transparent bg-clip-text bg-gradient-to-tr mb-20 from-purple-500 via-purple-600 to-blue-300">
         What would you like to learn?
       </h1>
       <div>
-        <Filter />
-        <div className=" flex justify-center items-center w-full mt-6 mb-2">
+        <div className=" flex justify-center items-center gap-3.5 w-full mt-6 mb-2">
           <SearchBar />
+          <Filter />
         </div>
         <div>
           <div>
-            <div className="hidden max-md:block mt-10 mr-5">
-              <Link to={"/courses/list"}>
-                <div className=" text-sm font-normal text-purple-600 flex justify-self-end gap-1">
-                  <span>view all</span>
-                  <RxArrowRight className=" text-lg" />
-                </div>
-              </Link>
-            </div>
+            {courses?.length != 0 && (
+              <div className="hidden max-md:block mt-10 mr-5">
+                <Link to={"/courses/list"}>
+                  <div className=" text-sm font-normal text-purple-600 flex justify-self-end gap-1">
+                    <span>view all</span>
+                    <RxArrowRight className=" text-lg" />
+                  </div>
+                </Link>
+              </div>
+            )}
             <div className=" grid grid-cols-4 place-items-center max-xl:grid-cols-3  max-lg:grid-cols-2 max-md:flex max-md:gap-4 max-md:overflow-auto max-md:pl-3">
               {courses &&
                 courses.map((course) => {
@@ -55,14 +45,18 @@ function Courses() {
                 })}
             </div>
             <div>
-              {!courses ? (
+              {courses?.length == 0 && (
                 <div className=" grid grid-cols-4 place-items-center  max-lg:grid-cols-2 max-md:grid-cols-4 gap-1 max-md:gap-72 max-md:pl-36 max-md:overflow-auto ">
                   <SkeletonCard />
                   <SkeletonCard />
                   <SkeletonCard />
                   <SkeletonCard />
+                  <SkeletonCard />
+                  <SkeletonCard />
+                  <SkeletonCard />
+                  <SkeletonCard />
                 </div>
-              ) : null}
+              )}
             </div>
           </div>
         </div>
